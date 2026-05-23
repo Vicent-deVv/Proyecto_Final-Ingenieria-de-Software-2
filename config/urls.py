23cd20b11d8530b5aa1d785a9cf598dd3d379235
VicentDev <175661774+Vicent-deVv@users.noreply.github.com>
2026-05-23 10:41:01 -0500

    feat: Se agrego el enrutamiento inicial y las views de cada django app

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('usuarios/',include('apps.usuarios.urls')),
+    path('pedidos/',include('apps.pedidos.urls')),
+    path('facturacion/',include('apps.facturacion.urls')),
+    path('almacen/',include('apps.almacen.urls')),
+    path('abastecimiento/',include('apps.abastecimiento.urls'))
 ]
